cc12c3dedd5873e42c940f315267ba57c8af69ba
ZH <99233740+KochoTawamure@users.noreply.github.com>
2022-03-07 15:34:44 +0800

    Add files via upload

--- pages/applicant_registration.php
+++ pages/applicant_registration.php
@@ -11,9 +11,9 @@
                
          <form action="../PHP/register_applicant.php" method = "post">
             <label>Fullname  :</label><input type = "text" name = "fullname" class = "box"/><br /><br />
-            <label>Income  :</label><input type = "text" name = "income" class = "box"/><br /><br />
+            <label>Income  :</label><input type = "number" name = "income" class = "box"/><br /><br />
             <label>Address  :</label><input type = "text" name = "address" class = "box"/><br /><br />
-            <label>Mobile No.  :</label><input type = "text" name = "mobileNo" class = "box"/><br /><br />
+            <label>Mobile No.  :</label><input type = "number" name = "mobileNo" class = "box"/><br /><br />
             <label>File name  :</label><input type = "text" name = "filename" class = "box" /><br/><br />
             <label>Description of file  :</label><input type = "text" name = "description" class = "box" /><br/><br />
             <input type = "submit" value = " Submit "/><br />
