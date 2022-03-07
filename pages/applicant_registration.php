94f410d74268d355fff09bf5406c61867b11076a
ZH <99233740+KochoTawamure@users.noreply.github.com>
2022-03-07 14:23:09 +0800

    Add files via upload

---
+++ pages/applicant_registration.php
@@ -0,0 +1,26 @@
+<html>
+   <head>
+      <title>Register Aid Applicant</title>  
+   </head>
+   
+   <body>
+	
+   <div>  
+      <div><b>Register Aid Applicant</b></div>	
+      <div>
+               
+         <form action="../PHP/register_applicant.php" method = "post">
+            <label>Fullname  :</label><input type = "text" name = "fullname" class = "box"/><br /><br />
+            <label>Income  :</label><input type = "text" name = "income" class = "box"/><br /><br />
+            <label>Address  :</label><input type = "text" name = "address" class = "box"/><br /><br />
+            <label>Mobile No.  :</label><input type = "text" name = "mobileNo" class = "box"/><br /><br />
+            <label>File name  :</label><input type = "text" name = "filename" class = "box" /><br/><br />
+            <label>Description of file  :</label><input type = "text" name = "description" class = "box" /><br/><br />
+            <input type = "submit" value = " Submit "/><br />
+         </form>
+               		
+      </div>	
+   </div>
+
+   </body>
+</html>
