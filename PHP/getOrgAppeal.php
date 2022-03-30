2eec60cd0fa4f61ce7add2758b907656c227a3c2
ZH <99233740+KochoTawamure@users.noreply.github.com>
2022-03-30 22:43:23 +0800

    Create getOrgAppeal.php

---
+++ PHP/getOrgAppeal.php
@@ -0,0 +1,20 @@
+<?php 
+include "config.php";
+$orgID = $_SESSION['orgID'];
+
+$sql = "SELECT * from tb_appeal WHERE orgID=$orgID AND toDate >= CAST(CURRENT_TIMESTAMP AS DATE)";
+$result = $conn-> query($sql);
+if (mysqli_num_rows($result) > 0){
+    while ($row= $result-> fetch_assoc()){
+        echo "<tr>";
+        echo "<td>" . $row['appealID'] . "</td>";
+        echo "<td>" . $row['fromDate'] . "</td>";
+        echo "<td>" . $row['toDate'] . "</td>";
+        echo "<td>" . $row['description'] . "</td>";
+        echo "<td>" . $row['outcome'] . "</td>";
+        echo "</tr>";
+    }
+}else{
+    echo"no content";
+}
+?>
