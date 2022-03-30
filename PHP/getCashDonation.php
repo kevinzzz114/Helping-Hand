f0e08d9bcd024cfdfaa46bdb6d919d64ee24f21e
ZH <99233740+KochoTawamure@users.noreply.github.com>
2022-03-30 22:43:18 +0800

    Create getCashDonation.php

---
+++ PHP/getCashDonation.php
@@ -0,0 +1,20 @@
+<?php 
+include "config.php";
+$appealID = $_SESSION['appealID'];
+$sql = "SELECT * from (tb_contribution INNER JOIN tb_cash_donation ON tb_contribution.contributionID = tb_cash_donation.contributionID)
+ WHERE appealID = $appealID";
+$result = $conn-> query($sql);
+if (mysqli_num_rows($result) > 0){
+    while ($row= $result-> fetch_assoc()){
+        echo "<tr>";
+        echo "<td>" . $row['contributionID'] . "</td>";
+        echo "<td>" . $row['receivedDate'] . "</td>";
+        echo "<td>" . $row['amount'] . "</td>";
+        echo "<td>" . $row['paymentChannel'] . "</td>";
+        echo "<td>" . $row['referenceNo'] . "</td>";
+        echo "</tr>";
+    }
+}else{
+    echo"no content";
+}
+?>
