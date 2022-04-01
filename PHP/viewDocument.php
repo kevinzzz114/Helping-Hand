b10f9b3385a314fca961e45c35dab1a7d14a4e0d
ZH <99233740+KochoTawamure@users.noreply.github.com>
2022-04-01 14:52:58 +0800

    Update viewDocument.php

--- PHP/viewDocument.php
+++ PHP/viewDocument.php
@@ -9,20 +9,22 @@ function validate($data){
     return $data;
 }
 
-$username = validate($_POST['username']);
-$checkUsername = "SELECT * FROM (tb_applicant INNER JOIN tb_document ON tb_applicant.IDno = tb_document.IDno)
- WHERE username='$username'";
-$result = mysqli_query($conn, $checkEmail);
-if (mysqli_num_rows($result) != 1){
-    echo "<script>alert('Invald username'); window.location.href='../pages/recordDisbursement2.php';</script>";
+$ID = validate($_POST['ID']);
+$checkID = "SELECT * FROM (tb_document INNER JOIN tb_applicant ON tb_applicant.IDno = tb_document.IDno)
+ WHERE tb_document.IDno='$ID'";
+$result = mysqli_query($conn, $checkID);
+if (mysqli_num_rows($result) < 1){
+    //echo "<script>window.open('http://localhost/Helping-Hand/uploads/user8hh.png');</script>";
+    echo "<script>alert('Invald applicant ID or applicant has no documents'); window.location.href='../pages/recordDisbursement2.php';</script>";
 }else{
-    $row= $result-> fetch_assoc();
     while ($row= $result-> fetch_assoc()){
+        $username = $row['username'];
         $filename = $row['filename'];
-        $path = "uploads/$filename"."*";
+        $path = "../uploads/$username"."$filename"."*";
         $fileInfo = glob($path);
         $fileActualname = $fileInfo[0];
-        echo "<script>window.open(http://localhost/Helping-Hand/uploads/$fileActualname)';</script>";
+        echo "<script>window.open('http://localhost/Helping-Hand/uploads/$fileActualname');</script>";
     }
+    echo "<script>window.location.href='../pages/recordDisbursement2.php';</script>";
 }
 ?>
