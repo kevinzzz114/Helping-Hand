215ae1deb9a576174291256f671ec3395fd62689
ZH <99233740+KochoTawamure@users.noreply.github.com>
2022-04-01 14:52:52 +0800

    Update redirectContributionApplicant.php

--- PHP/redirectContributionApplicant.php
+++ PHP/redirectContributionApplicant.php
@@ -3,7 +3,7 @@ include "config.php";
 session_start();
 $orgID = $_SESSION['orgID'];
 $appealID = $_POST['appealID'];
-$checkID = "SELECT * FROM tb_temp_applicant WHERE appealID ='$appealID' AND orgID = '$orgID'";
+$checkID = "SELECT * FROM tb_appeal WHERE appealID ='$appealID' AND orgID = '$orgID'";
 $ID = mysqli_query($conn, $checkID);
 if (mysqli_num_rows($ID) != 1){
     echo "<script>alert('Invalid Appeal ID'); window.location.href='../pages/recordDisbursement.php';</script>";
@@ -12,7 +12,7 @@ if (mysqli_num_rows($ID) != 1){
     if (isset($_POST['showContribution'])){
         echo "<script>window.location.href='../pages/viewContribution.php';</script>";
     }else{
-        echo "<script>window.location.href='../pages/disbursement.php';</script>";
+        echo "<script>window.location.href='../pages/recordDisbursement2.php';</script>";
     }
 }
 ?>
